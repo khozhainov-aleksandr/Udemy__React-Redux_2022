2569bdf8c91523a7a7b29e1d0f8a99b37bf5723d
Aleksandr Khozhainov <khozhainov.aleksandr@gmail.com>
2022-09-10 00:12:08 +0300

    Finish

--- src/App.js
+++ src/App.js
@@ -1,7 +1,29 @@
 import React from 'react';
+import {useSelector, useDispatch} from 'react-redux';
+import { increment, decrement, reset } from './app/store';
+import './App.css'; 
 
 export default function App() {
+  const counter = useSelector(state => state);
+  const dispatch = useDispatch();
+
   return (
-    <h1>Redux 2022</h1>
+    <div className="App">
+      <h1>Redux 2022</h1>
+
+      <h3>{counter}</h3>
+
+      <button onClick={() => dispatch(increment)}>
+        +
+      </button>
+
+      <button onClick={() => dispatch(decrement)}>
+        -
+      </button>
+
+      <button onClick={() => dispatch(reset)}>
+        reset
+      </button>
+    </div>
   );
 }
--- src/app/store.js
+++ src/app/store.js
@@ -1,8 +1,25 @@
-import { configureStore } from '@reduxjs/toolkit';
-import counterReducer from '../features/counter/counterSlice';
+import { legacy_createStore as createStore } from "redux";
 
-export const store = configureStore({
-  reducer: {
-    counter: counterReducer,
-  },
-});
+const counter = (state = 0, action) => {
+  switch (action.type) {
+    case "INCREMENT": {
+      return state + 1;
+    }
+    case "DECREMENT": {
+      return state - 1;
+    }
+    case "RESET": {
+      return 0;
+    }
+    default: {
+      return state;
+    }
+  }
+};
+
+export const store = createStore(counter);
+
+// actions
+export const increment = { type: "INCREMENT" };
+export const decrement = { type: "DECREMENT" };
+export const reset = { type: "RESET" };
