be6b1e7e7cba3e0d3daae36393d6c97df04f5094
Aleksandr Khozhainov <khozhainov.aleksandr@gmail.com>
2022-09-10 02:04:27 +0300

    Done

--- src/App.js
+++ src/App.js
@@ -1,10 +1,48 @@
 import React from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { addTodo, deleteTodo, toggleTodo } from './app/store';
 import './App.css';
 
 export default function App() {
+  const todos = useSelector((state) => state);
+  const dispatch = useDispatch();
+
+  console.log(todos);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    dispatch(addTodo(e.target.title.value)); // передается значение из input с name === title
+    e.target.reset();
+  }
+
   return (
     <div className='App'>
       <h1>Redux 2022</h1>
+
+      <form onSubmit={handleSubmit}>
+        <input type="text" name="title" placeholder="New todo" />
+        <button type="submit">
+          Add new todo
+        </button>
+      </form>
+
+      <ul>
+        {todos.map(todo => (
+          <li key={todo.id}>
+            <input
+              type="checkbox"
+              checked={todo.completed}
+              onClick={() => dispatch(toggleTodo(todo.id))}
+            />
+              {todo.title}
+            <button
+              onClick={() => dispatch(deleteTodo(todo.id))}
+            >
+              Delete
+            </button>
+          </li>
+        ))}
+      </ul>
     </div>
   );
 }
--- src/app/store.js
+++ src/app/store.js
@@ -1,7 +1,44 @@
 import { legacy_createStore as  createStore } from "redux";
 
-const x = (state, action) => {
-  return state;
+const todo = (state = [], action) => {
+  switch (action.type) {
+    case 'ADD_TODO':
+      return [
+        ...state,
+        {
+          id: Date.now(),
+          title: action.title,
+          completed: false,
+        }
+      ];
+    
+    case 'DELETE_TODO':
+      return state.filter((todo) => todo.id !== action.id);
+
+    case 'TOGGLE_TODO':
+      return state.map((todo) => (
+        todo.id === action.id
+        ? {...todo, completed: !todo.completed}
+        : todo
+      ));
+  
+    default: return state;
+  }
 }
 
-export const store = createStore(x);
+export const store = createStore(todo, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
+
+export const addTodo = (title) => ({
+  type: 'ADD_TODO',
+  title,
+});
+
+export const deleteTodo = (id) => ({
+  type: 'DELETE_TODO',
+  id,
+});
+
+export const toggleTodo = (id) => ({
+  type: 'TOGGLE_TODO',
+  id,
+});
